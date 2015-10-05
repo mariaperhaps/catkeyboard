92c658921b5719528165b92c6e093dd374c02ecd
mariaperhaps <mariatschettino@gmail.com>
2015-10-05 11:55:44 -0400

    Update catsongs.js

--- public/scripts/catsongs.js
+++ public/scripts/catsongs.js
@@ -27,32 +27,31 @@ function opacityChange (div){
 }
 
   window.addEventListener("keypress", function(e){
-    // var string = String.fromCharCode(e.keyCode)
-     if (e.keyCode == 97) {
+     if (e.keyCode === 97) {
        playCat('step_01');
        opacityChange('cat_01');
-     } else if(e.keyCode == 115){
+     } else if(e.keyCode === 115){
         playCat('step_02');
         opacityChange('cat_02');
-     } else if(e.keyCode == 100){
+     } else if(e.keyCode === 100){
         playCat('step_03');
         opacityChange('cat_03')
-     } else if(e.keyCode == 102){
+     } else if(e.keyCode === 102){
         playCat('step_04');
         opacityChange('cat_04');
-     } else if(e.keyCode == 106){
+     } else if(e.keyCode === 106){
         playCat('step_05');
         opacityChange('cat_05');
-     } else if(e.keyCode == 107){
+     } else if(e.keyCode === 107){
         playCat('step_06');
         opacityChange('cat_06');
-     } else if(e.keyCode == 108){
+     } else if(e.keyCode === 108){
         playCat('step_07');
         opacityChange('cat_07');
-     } else if(e.keyCode == 59){
+     } else if(e.keyCode === 59){
         playCat('step_08');
         opacityChange('cat_08');
-     } else if(e.keyCode == 32){
+     } else if(e.keyCode === 32){
         playCat('thelongmeow');
         opacityChange('long_meow');
      }
